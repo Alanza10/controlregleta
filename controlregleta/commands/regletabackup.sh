f225da9f2d638dca1ef390d5a6ae946b5c814bcb
root <root@eeepc.alanza10.dyndns.org>
2013-10-02 22:10:58 +0200

    p

--- controlregleta/commands/regletabackup.sh
+++ controlregleta/commands/regletabackup.sh
@@ -1,20 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #source /etc/controlregleta.cfg
-i=0
-while read line; do
-  if [[ "$line" =~ ^[^#]*= ]]; then
-    name[i]=${line%% =*}
-    value[i]=${line#*= }
-    ((i++))
-  fi
-done < /etc/controlregleta.cfg
-
-echo "total array elements: ${#name[@]}"
-echo "name[0]: ${name[0]}"
-echo "value[0]: ${value[0]}"
-echo "name[1]: ${name[1]}"
-echo "value[1]: ${value[1]}"
-echo "name array: ${name[@]}"
-echo "value array: ${value[@]}"
+function cfg_parser(){
+	eval "$(echo "$ttay")" # eval the result
+}
+ttay="tty" 
+cfg_parser
 #sh /home/pi/cs/controlregleta/commands/programar.sh $tty $relay1 $prog1
 	
