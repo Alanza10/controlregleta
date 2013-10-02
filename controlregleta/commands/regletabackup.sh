8d8387f32b42e8064f76a62a7f34f7f7c6ebee8e
root <root@eeepc.alanza10.dyndns.org>
2013-10-02 02:14:04 +0200

    sss

--- controlregleta/commands/regletabackup.sh
+++ controlregleta/commands/regletabackup.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
-source /etc/controlregleta.cfg
-sh /home/pi/cs/controlregleta/commands/programar.sh $tty $relay1 $prog1
+#!/bin/sh
+#source /etc/controlregleta.cfg
+i=0
+while read line; do
+  if [[ "$line" =~ ^[^#]*= ]]; then
+    name[i]=${line%% =*}
+    value[i]=${line#*= }
+    ((i++))
+  fi
+done < /etc/controlregleta.cfg
+
+echo "total array elements: ${#name[@]}"
+echo "name[0]: ${name[0]}"
+echo "value[0]: ${value[0]}"
+echo "name[1]: ${name[1]}"
+echo "value[1]: ${value[1]}"
+echo "name array: ${name[@]}"
+echo "value array: ${value[@]}"
+#sh /home/pi/cs/controlregleta/commands/programar.sh $tty $relay1 $prog1
 	
