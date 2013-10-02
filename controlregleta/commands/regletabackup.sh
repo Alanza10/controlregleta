7b758c029be552f8424b8bc76c9461b5249a7cf9
root <root@eeepc.alanza10.dyndns.org>
2013-10-03 00:45:13 +0200

    soporte fichero configuracion mejorado

--- controlregleta/commands/regletabackup.sh
+++ controlregleta/commands/regletabackup.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
-#source /etc/controlregleta.cfg
-function cfg_parser(){
-	eval "$(echo "$ttay")" # eval the result
-}
-ttay="tty" 
-cfg_parser
-#sh /home/pi/cs/controlregleta/commands/programar.sh $tty $relay1 $prog1
+CONFIGFILE="/etc/controlregleta.cfg"
+
+#defaults
+TTY=/dev/ttyUSB0
+RELAY1=0
+RELAY2=0
+RELAY3=0
+RELAY4=0
+PROG1=000000000000
+PROG2=000000000000
+PROG3=000000000000
+PROG4=000000000000
+
+#volcado de las configuraciones
+eval `sed '/^ *#/d;s/=/ /;' < "$CONFIGFILE" |  
+while read key val
+do
+    str="$key='$val'"   
+     echo "$str"
+done`
+
+if [ "$RELAY1" -eq 1  ]; then 
+	sh /home/pi/cs/controlregleta/commands/programar.sh $TTY $RELAY1 $PROG1
+fi
+
+if [ "$RELAY2" -eq 2  ]; then 
+	sh /home/pi/cs/controlregleta/commands/programar.sh $TTY $RELAY2 $PROG2
+fi
+
+if [ "$RELAY3" -eq 3  ]; then 
+	sh /home/pi/cs/controlregleta/commands/programar.sh $TTY $RELAY4 $PROG2
+fi
+
+if [ "$RELAY4" -eq 4  ]; then 
+	sh /home/pi/cs/controlregleta/commands/programar.sh $TTY $RELAY4 $PROG4
+fi
 	
